f79e473ffe37e335f68a676d4627521e72c94863
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2026-03-23 13:41:27 +0800

    The `PROJECTS` constant variable is a list of projects loaded from a json file `projects.json` via the `load_projects` util function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import air
+from utils import PROJECTS
 
 app = air.Air()
 
 
 @app.page
 def index(request: air.Request):
-    return app.jinja(request, "index.html", title="PythonAsia Open Source")
+    return app.jinja(request, "index.html", title="PythonAsia Open Source", projects=PROJECTS)
